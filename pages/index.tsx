8c671b26c4aca5222f5762bbb5c844cbaad17134
lucianosants <lucianola18@gmail.com>
2022-12-14 15:12:11 -0300

    style: update in inndex page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,9 @@ export default function Home({ posts }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const posts = await getAllPosts();
+    const posts = await getAllPosts(
+        'deep&sort=id:desc&pagination[start]=0&pagination[limit]=30'
+    );
 
     return {
         props: { posts },
