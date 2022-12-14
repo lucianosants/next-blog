60bc1e56d06f0c9723037485c5b9a6ec42f20f72
lucianosants <lucianola18@gmail.com>
2022-12-14 15:11:22 -0300

    style: sort posts

--- src/data/posts/get-all-posts.ts
+++ src/data/posts/get-all-posts.ts
@@ -2,9 +2,10 @@ import { PostData } from '../../domain/posts/post';
 import { fetchJson } from '../../services/fetchJson';
 
 const env = process.env.API_POSTS;
+// const sort = process.env.API_SORT_POSTS; // alternative to sort without query in function
 
-export const getAllPosts = async (): Promise<PostData[]> => {
-    const url = `${env}`;
+export const getAllPosts = async (query: string): Promise<PostData[]> => {
+    const url = `${env}&${query}`;
     const posts = await fetchJson<PostData[]>(url);
     return posts;
 };
