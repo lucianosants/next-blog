32a0324ab0af80e93ed2755f76e5f72227a329ee
lucianosants <lucianola18@gmail.com>
2022-12-15 09:21:40 -0300

    feat: add get post to pages

---
+++ src/data/posts/get-post.ts
@@ -0,0 +1,16 @@
+import { PostData } from '../../domain/posts/post';
+import { fetchJson } from '../../services/fetchJson';
+
+const env = process.env.API_POST_SLUG;
+// const sort = process.env.API_SORT_POSTS; // alternative to sort without query in function
+
+export const getPost = async (slug: string | string[]): Promise<PostData[]> => {
+    const slugString = Array.isArray(slug) ? slug[0] : slug;
+    const url = `${env}?filters[slug]=${slug}&populate=%2A`;
+
+    // new structure to Strapi v4
+    // https://strapi-production-0af2.up.railway.app/api/posts ?filters[slug]= my-slug &populate=%2A
+
+    const jsonPosts = await fetchJson<PostData[]>(url);
+    return jsonPosts;
+};
