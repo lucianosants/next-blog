5ccbe9e0e80d3f557ad1a0c8fe3280fcf5ed2c14
lucianosants <lucianola18@gmail.com>
2022-12-15 09:24:12 -0300

    feat: add page slug for each post

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,43 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { getAllPosts } from '../../src/data/posts/get-all-posts';
+import { getPost } from '../../src/data/posts/get-post';
+import { PostData } from '../../src/domain/posts/post';
+
+export interface DynamicPostProps {
+    post: PostData;
+}
+
+export interface IParams extends ParsedUrlQuery {
+    slug: string;
+}
+
+export default function DynamicPosts({ post }: DynamicPostProps) {
+    // console.log(post);
+    return <div>titulo: {post.attributes.title}</div>;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const posts = await getAllPosts();
+
+    return {
+        paths: posts.map((post) => {
+            return {
+                params: {
+                    slug: post.attributes.slug,
+                },
+            };
+        }),
+        fallback: true,
+    };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { slug } = ctx.params as IParams;
+
+    const posts = await getPost(slug);
+
+    return {
+        props: { post: posts[0] },
+    };
+};
