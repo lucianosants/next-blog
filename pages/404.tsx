7e5f42d947d43b5b4f8a6f4785f2e6824212c198
lucianosants <lucianola18@gmail.com>
2022-12-15 09:19:13 -0300

    feat: add page 404

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Styled404 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid red;
+    height: 100%;
+    gap: 7rem;
+    img {
+        width: 25rem;
+    }
+`;
+
+export default function PageNotFound() {
+    return (
+        <Styled404>
+            <img src="/page-not-found.svg" alt="" />
+            <p>PAGE NOT FOUND</p>
+        </Styled404>
+    );
+}
