f5d50f550864f2170279098f251d88ddd2af4976
lucianosants <lucianola18@gmail.com>
2022-12-15 09:20:40 -0300

    style: update query param

--- src/data/posts/get-all-posts.ts
+++ src/data/posts/get-all-posts.ts
@@ -4,7 +4,7 @@ import { fetchJson } from '../../services/fetchJson';
 const env = process.env.API_POSTS;
 // const sort = process.env.API_SORT_POSTS; // alternative to sort without query in function
 
-export const getAllPosts = async (query: string): Promise<PostData[]> => {
+export const getAllPosts = async (query = ''): Promise<PostData[]> => {
     const url = `${env}&${query}`;
     const posts = await fetchJson<PostData[]>(url);
     return posts;
