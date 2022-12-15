db50841ab72df7388a0ad033a263132557f57e08
lucianosants <lucianola18@gmail.com>
2022-12-15 15:08:33 -0300

    style: render post cover in post page

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -3,6 +3,7 @@ import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Heading from '../../components/Heading';
 import { MainContainer } from '../../components/MainContainer';
+import PostCover from '../../components/PostCover';
 import { PostData } from '../../domain/posts/post';
 
 interface IPostProps {
@@ -10,11 +11,18 @@ interface IPostProps {
 }
 
 export const Post = ({ post }: IPostProps) => {
+    console.log(post);
     return (
         <>
             <Header />
             <MainContainer>
                 <Heading>{post?.attributes.title}</Heading>
+                <PostCover
+                    src={
+                        post?.attributes.cover.data.attributes.formats.large.url
+                    }
+                    alt={`Image from post ${post?.attributes.title}`}
+                />
                 <div dangerouslySetInnerHTML={{ __html: post?.content }} />
             </MainContainer>
             <Footer />
