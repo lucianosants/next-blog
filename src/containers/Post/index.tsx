948cfd0fbd8aef71ced79e041a842ade0b03ca08
lucianosants <lucianola18@gmail.com>
2022-12-16 15:30:50 -0300

    style: render comments components in post page

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -1,3 +1,4 @@
+import Comments from '../../components/Comments';
 import Heading from '../../components/Heading';
 import { MainContainer } from '../../components/MainContainer';
 import PostContainer from '../../components/PostContainer';
@@ -27,6 +28,10 @@ export const Post = ({ post }: IPostProps) => {
                     category={post?.attributes.category.data.attributes.name}
                 />
                 <PostContainer content={post?.content} />
+                <Comments
+                    slug={post?.attributes.slug}
+                    title={post?.attributes.title}
+                />
             </MainContainer>
         </>
     );
