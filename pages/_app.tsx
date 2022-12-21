92a070737c695d3ca21f4dd93f0e03ae5dec19f3
lucianosants <lucianola18@gmail.com>
2022-12-21 09:09:07 -0300

    feat: setup NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import { ThemeProvider } from 'styled-components';
 import Footer from '../src/components/Footer';
 import Header from '../src/components/Header';
 
 import { GlobalStyles } from '../src/styles/globalstyles';
 import { theme } from '../src/styles/theme';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
